78fcf63570e2730f57396672da58479235da7d9d
Dante Vicenzo <dantevcz@gmail.com>
2023-09-01 14:11:40 -0300

    endpoint 7 -> Transferir

--- src/controllers/transacoes.js
+++ src/controllers/transacoes.js
@@ -21,7 +21,6 @@ const depositar = (req, res) => {
     conta.saldo += Number(valor)
 
     const data = format(new Date(), "yyyy-MM-dd HH:mm:ss")
-    console.log(data)
 
     bancoDeDados.depositos.push({
         data,
@@ -59,13 +58,15 @@ const sacar = (req, res) => {
         return res.status(400).json({ mensagem: "Senha inválida!" })
     }
 
-    const saldoEhSuficiente = conta.saldo <= Number(valor)
+    const saldoEhSuficiente = conta.saldo >= Number(valor)
 
     if (!saldoEhSuficiente) {
         return res.status(400).json({ mensagem: "Saldo insuficiente!" })
     }
 
-    conta.saldo -= Number(saldo)
+    conta.saldo -= Number(valor)
+
+    const data = format(new Date(), "yyyy-MM-dd HH:mm:ss")
 
     bancoDeDados.saques.push({
         data,
@@ -77,7 +78,58 @@ const sacar = (req, res) => {
 }
 
 const transferir = (req, res) => {
-    res.json({ mensagem: "Rota de transferência" })
+    const { numero_conta_origem, numero_conta_destino, valor, senha } = req.body
+
+    if (numero_conta_origem === undefined || numero_conta_destino === undefined || valor === undefined) {
+        return res.status(400).json({ mensagem: "É obrigatório informar o número da conta de origem, número da conta de destino e o valor!" })
+    }
+
+    if (!senha) {
+        return res.status(400).json({ mensagem: "Informe a senha da conta de origem!" })
+    }
+
+    if (Number(valor) <= 0) {
+        return res.status(400).json({ mensagem: "Não é permitido fazer transferência com valores negativos ou zerados." })
+    }
+
+    let contaOrigem = bancoDeDados.contas.find((conta) => conta.numero === Number(numero_conta_origem))
+    let contaDestino = bancoDeDados.contas.find((conta) => conta.numero === Number(numero_conta_destino))
+
+    if (!contaOrigem) {
+        return res.status(400).json({ mensagem: "A conta de origem informada não existe!" })
+    }
+
+    if (!contaDestino) {
+        return res.status(400).json({ mensagem: "A conta de destino informada não existe!" })
+    }
+
+    const senhaEhValida = contaOrigem.usuario.senha === senha
+
+    if (!senhaEhValida) {
+        return res.status(400).json({ mensagem: "Senha inválida!" })
+    }
+
+    console.log(contaOrigem)
+
+    const saldoEhSuficiente = contaOrigem.saldo >= Number(valor)
+
+    if (!saldoEhSuficiente) {
+        return res.status(400).json({ mensagem: "Saldo insuficiente!" })
+    }
+
+    contaOrigem.saldo -= Number(valor)
+    contaDestino.saldo += Number(valor)
+
+    const data = format(new Date(), "yyyy-MM-dd HH:mm:ss")
+
+    bancoDeDados.transferencias.push({
+        data,
+        numero_conta_origem,
+        numero_conta_destino,
+        valor
+    })
+
+    return res.status(204).send()
 }
 
 module.exports = {
