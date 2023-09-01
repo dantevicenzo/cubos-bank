588bf054c726a403aa1952d4a5ec1c844bc3108f
Dante Vicenzo <dantevcz@gmail.com>
2023-09-01 13:59:42 -0300

    endpoint 6 -> sacar

--- src/controllers/transacoes.js
+++ src/controllers/transacoes.js
@@ -33,7 +33,47 @@ const depositar = (req, res) => {
 }
 
 const sacar = (req, res) => {
-    res.json({ mensagem: "Rota de saque" })
+    const { numero_conta, valor, senha } = req.body
+
+    if (numero_conta === undefined || valor === undefined) {
+        return res.status(400).json({ mensagem: "O número da conta e o valor são obrigatórios!" })
+    }
+
+    if (!senha) {
+        return res.status(400).json({ mensagem: "Informe a senha da conta!" })
+    }
+
+    if (Number(valor) <= 0) {
+        return res.status(400).json({ mensagem: "Não é permitido fazer saques com valores negativos ou zerados." })
+    }
+
+    const conta = bancoDeDados.contas.find((conta) => conta.numero === Number(numero_conta))
+
+    if (!conta) {
+        return res.status(400).json({ mensagem: "A conta informada não existe!" })
+    }
+
+    const senhaEhValida = conta.usuario.senha === senha
+
+    if (!senhaEhValida) {
+        return res.status(400).json({ mensagem: "Senha inválida!" })
+    }
+
+    const saldoEhSuficiente = conta.saldo <= Number(valor)
+
+    if (!saldoEhSuficiente) {
+        return res.status(400).json({ mensagem: "Saldo insuficiente!" })
+    }
+
+    conta.saldo -= Number(saldo)
+
+    bancoDeDados.saques.push({
+        data,
+        numero_conta,
+        valor
+    })
+
+    return res.status(204).send()
 }
 
 const transferir = (req, res) => {
